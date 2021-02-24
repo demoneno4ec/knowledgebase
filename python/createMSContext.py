52d2e40666fd0786200dfc3094548addfe640380
mpestov <maxim.pestov@bfb.one>
2021-02-24 17:48:13 +0300

    (update)[python-createContextMs]{Сделал обновление в плане вынесения функций}

--- python/createMSContext.py
+++ python/createMSContext.py
@@ -1,6 +1,39 @@
 import sys
 import os
+import types
 from termcolor import colored, cprint
+
+##calbacks
+def fileChangeProjectMark(line):
+    return [line.replace('#PROJECT_NAME#', project)]
+
+def dockerComposePreparation(line):
+    lines = []
+
+    if ('#PLACE_NGINX_VOLUME#' in line):
+        with open('./templates/docker-compose/volumes/nginx_volume', 'r') as file:
+            for line in file:
+                lines.append(line)
+    elif('#PLACE_NEW_MICROSERVICE#' in line):
+        with open('./templates/docker-compose/container', 'r') as file:
+            for line in file:
+                lines.append(line)
+    else:
+        lines.append(line)
+    return lines
+
+## functions
+def fileChangeAndMoveWithCallbackChange(fromPath, toPath, callback):
+    lines = []
+    # Parse the file into lines
+    with open(fromPath, 'r') as file:
+        for line in file:
+            if(callable(callback)):
+                lines += callback(line)
+
+    # Write them back to the file
+    with open(toPath, 'w') as file:
+        file.writelines(lines)
 
 error = lambda x: cprint(x, 'white', 'on_red')
 info = lambda x: cprint(x, 'green')
@@ -13,29 +46,7 @@ except IndexError:
     print("python3 createMicroserviceContext.py project")
     exit()
 
-
-
-dockerLines = []
-
-
-
-# Parse the file into lines
-with open('./docker-compose.yml', 'r') as file:
-    for line in file:
-        if ('#PLACE_NGINX_VOLUME#' in line):
-            with open('./templates/docker-compose/volumes/nginx_volume', 'r') as file:
-                for line in file:
-                    dockerLines.append(line)
-        elif('#PLACE_NEW_MICROSERVICE#' in line):
-            with open('./templates/docker-compose/container', 'r') as file:
-                for line in file:
-                    dockerLines.append(line)
-        else:
-            dockerLines.append(line)
-
-# Write them back to the file
-with open('./templates/docker-compose/docker-compose.yml', 'w') as file:
-    file.writelines(dockerLines)
+fileChangeAndMoveWithCallbackChange('./docker-compose.yml', './templates/docker-compose/docker-compose.yml', dockerComposePreparation)
 
 configs = [
     {
@@ -50,19 +61,8 @@ configs = [
         'from': './templates/docker-compose/docker-compose.yml',
         'to' : './docker-compose.yml'
     },
-
 ]
-
 
 for config in configs:
     if (os.path.isfile(config['from'])):
-        lines = []
-
-        # Parse the file into lines
-        with open(config['from'], 'r') as file:
-            for line in file:
-                lines.append(line.replace('#PROJECT_NAME#', project))
-
-        # Write them back to the file
-        with open(config['to'], 'w') as file:
-            file.writelines(lines)
+        fileChangeAndMoveWithCallbackChange(config['from'], config['to'], fileChangeProjectMark)
